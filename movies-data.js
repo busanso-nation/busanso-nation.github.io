cd33e9545e88bd47a6e5e9c332c3794521335789
busansonation <libraryyafilm@gmail.com>
2026-01-25 05:18:07 -0800

    Create movies-data.js

---
+++ movies-data.js
@@ -0,0 +1,23 @@
+const MOVIES = [
+  {
+    id: "under-the-tree",
+    title: "UNDER THE TREE",
+    poster: "https://i.ibb.co/DPQGhfgn/Untitled-1.jpg",
+    video: "https://www.youtube.com/embed/dazxm7NXbqQ",
+    year: "2026",
+    country: "Japan",
+    duration: "1h 45m",
+    synopsis: "A mysterious drama unfolding beneath an ancient tree."
+  },
+
+  {
+    id: "kanor",
+    title: "KANOR",
+    poster: "https://i.ibb.co/Zphm4pNY/KANOR.jpg",
+    video: "https://www.youtube.com/embed/p31wdNO5ibQ",
+    year: "2026",
+    country: "India",
+    duration: "2h 10m",
+    synopsis: "A gripping tale of power, revenge, and destiny."
+  }
+];
