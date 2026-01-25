109dba302d424973ab2d6920b947582bbaafd885
busansonation <libraryyafilm@gmail.com>
2026-01-25 12:46:08 -0800

    Update movies-data.js

--- movies-data.js
+++ movies-data.js
@@ -1,6 +1,17 @@
 const MOVIES = [
   {
-    id: "under-the-tree",
+    id: "tiger",
+    title: "TIGER",
+    poster: "https://i.pinimg.com/736x/76/49/09/764909431a3d53f6a4e7229c0d49bde6.jpg",
+    video: "https://www.youtube.com/embed/dazxm7NXbqQ",
+    type: "free",
+    year: "2026",
+    country: "India",
+    duration: "1h 45m",
+    synopsis: "A mysterious drama unfolding beneath an ancient tree."
+  },
+    {
+    id: "underdtree",
     title: "UNDER THE TREE",
     poster: "https://i.ibb.co/DPQGhfgn/Untitled-1.jpg",
     video: "https://www.youtube.com/embed/dazxm7NXbqQ",
