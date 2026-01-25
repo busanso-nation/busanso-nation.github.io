695e53c7d8ff0836618384b68befcbfbc9d70202
busansonation <libraryyafilm@gmail.com>
2026-01-25 13:26:35 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -3,17 +3,18 @@ const SERIES = [
   {
     id: "tianlan",
     title: "LEGEND OF TIANLAN",
-    poster: "...",
-    genre: "Drama",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrUIaE2rY-37LS7sGriH_-Lk3Js-ATm_dj-Q&s",
+     genre: "Drama",
+    added: 000002,
+    type: "free",
     country: "China",
     year: "2025",
     synopsis: "A legendary journey of power and destiny between exorcists.",
     cast: ["Actor One", "Actor Two", "Actor Three"],
-
    episodes: [
   { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
   { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" }
-],
+]
 },
 
    {
