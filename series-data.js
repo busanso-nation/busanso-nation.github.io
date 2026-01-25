68402df2534758abb52ce6f089df53ee96b75a71
busansonation <libraryyafilm@gmail.com>
2026-01-25 06:38:05 -0800

    Create series-data.js

---
+++ series-data.js
@@ -0,0 +1,39 @@
+
+const SERIES = [
+  {
+    id: "tianlan",
+    title: "LEGEND OF TIANLAN",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPV71Q1KOpHSZMoqy0iqLy35OF1FAyYOAqxA&s",
+    genre: "Drama",
+    country: "China",
+    year: "2025",
+    synopsis: "A legendary journey of power and destiny between exorsits.",
+    cast: ["Actor One", "Actor Two", "Actor Three"],
+
+    episodesHTML: `
+      <ul>
+        <li><a href="https://www.youtube.com/embed/RlNqNyrRm_k">Episode 1</a></li>
+        <li><a href="https://www.youtube.com/embed/egF1sy2FPYs">Episode 2</a></li>
+        <li><a href="https://www.youtube.com/embed/UPrxaom2THM">Episode 3</a></li>
+      </ul>
+    `
+  },
+   {
+    id: "danzu",
+    title: "DANZU HERO",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRri6yH0P-l6UdDL3iKisvp1FqZpEADHTaw&s",
+    genre: "Drama",
+    country: "China",
+    year: "2024",
+    synopsis: "A legendary journey of power and destiny between exorsits.",
+    cast: ["Actor One", "Actor Two", "Actor Three"],
+
+    episodesHTML: `
+      <ul>
+        <li><a href="https://www.youtube.com/embed/RlNqNyrRm_k">Episode 1</a></li>
+        <li><a href="https://www.youtube.com/embed/egF1sy2FPYs">Episode 2</a></li>
+        <li><a href="https://www.youtube.com/embed/UPrxaom2THM">Episode 3</a></li>
+      </ul>
+    `
+  }
+];
