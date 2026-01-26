e29b2b6ded14a59d40a514c624bc2c7ec44c75fd
busansonation <libraryyafilm@gmail.com>
2026-01-26 01:20:24 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -30,7 +30,7 @@ const SERIES = [
     title: "AFFAIRS",
     poster: "https://pic5.iqiyipic.com/image/20250704/40/e6/v_183159476_m_601_374_0.jpg",
     genre: "love story",
-    added: 4,
+    added: 2,
     type: "free",
     country: "China",
     year: "2026",
@@ -46,7 +46,7 @@ const SERIES = [
     title: "HOT BLOODED",
     poster: "https://i.ibb.co/MxzB0DLC/Untitled-1.jpg",
     genre: "Action",
-    added: 5,
+    added: 3,
     type: "premium",
     country: "China",
     year: "2026",
@@ -64,6 +64,40 @@ const SERIES = [
   { title: "Episode 9", video: "https://www.youtube.com/embed/fWngGIZ8ZLk" },
   { title: "Episode 10", video: "https://www.youtube.com/embed/D0Op9g37vJs" },
   { title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" }
+      ]
+  },
+{
+    id: "marimar",
+    title: "MARIMAR",
+    poster: "https://serializd-tmdb-images.b-cdn.net/t/p/w500/oTw4YlgUEwqIp0zBxeyvNOMhLrn.jpg",
+    genre: "drama",
+    added: 4,
+    type: "premium",
+    country: "Pilippine",
+    year: "2026",
+    synopsis: "Marimar is a naive, poor young woman living in a beach hut with her grandparents. She falls in love with and marries Sergio Santibañez, whose wealthy family, especially his stepmother Angélica, humiliates her and drives them apart. After suffering tragic losses, Marimar transforms into the sophisticated Bella Aldama to seek revenge",
+    cast: ["Yiwei Zhou", "Meng Ziyi", "Peter Ho"],
+    episodes: [
+   { title: "Episode 1", video: "https://www.youtube.com/embed/NUz_6hcABnY" },
+  { title: "Episode 2", video: "https://www.youtube.com/embed/UcfIN1DHfLs" },
+  { title: "Episode 3", video: "https://www.youtube.com/embed/UtYFWoU_klw" },
+  { title: "Episode 4", video: "https://www.youtube.com/embed/db9D8yUzses" },
+  { title: "Episode 5", video: "https://www.youtube.com/embed/9Fd-zbhgOUM" },
+  { title: "Episode 6", video: "https://www.youtube.com/embed/KjCCe-YSWrI" },
+  { title: "Episode 7", video: "https://www.youtube.com/embed/jzAtePeQkTY" },
+  { title: "Episode 8", video: "https://www.youtube.com/embed/28cCahat4qs" },
+  { title: "Episode 9", video: "https://www.youtube.com/embed/NpUnrpVbQJU" },
+  { title: "Episode 10", video: "https://www.youtube.com/embed/aaQjedH7a24" },
+  { title: "Episode 11", video: "https://www.youtube.com/embed/wR4ItNRrtwQ" },
+  { title: "Episode 12", video: "https://www.youtube.com/embed/a4EPXOuLcN4" },
+  { title: "Episode 13", video: "https://www.youtube.com/embed/R9U18cXOtxI" },
+  { title: "Episode 14", video: "https://www.youtube.com/embed/SHcDDozXpzk" },
+  { title: "Episode 15", video: "https://www.youtube.com/embed/bcGn9W7Lyvk" },
+  { title: "Episode 16", video: "https://www.youtube.com/embed/veDLuzVQjhA" },
+  { title: "Episode 17", video: "https://www.youtube.com/embed/ZAFZr39JU7g" },
+  { title: "Episode 18", video: "https://www.youtube.com/embed/G8LzjrRLMBc" },
+  { title: "Episode 19", video: "https://www.youtube.com/embed/Eun8gufKszw" },
+  { title: "Episode 20", video: "https://www.youtube.com/embed/vrgxy7pCkC8" }
 
     ]
   }
