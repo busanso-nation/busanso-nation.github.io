338082694315e4c2ce0b538dd8911a1f3db0c178
busansonation <libraryyafilm@gmail.com>
2026-01-26 01:43:13 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -98,6 +98,27 @@ const SERIES = [
   { title: "Episode 18", video: "https://www.youtube.com/embed/G8LzjrRLMBc" },
   { title: "Episode 19", video: "https://www.youtube.com/embed/Eun8gufKszw" },
   { title: "Episode 20", video: "https://www.youtube.com/embed/vrgxy7pCkC8" }
+      ]
+  },
+{
+    id: "ashur",
+    title: "SPARTACUS HOUSE OF ASHUR",
+    poster: "https://i.ibb.co/RTD780Ry/MV5-BZWQ1-NDUw-Yj-Et-YTYz-YS00-YTc3-LTgz-OTEt-ZTM0-ZDhh-MGM3-ODAw-Xk-Ey-Xk-Fqc-Gc-V1.jpg",
+    genre: "Action",
+    added: 5,
+    type: "premium",
+    country: "USA",
+    year: "2026",
+    synopsis: "a 2025 Starz miniseries exploring an alternate timeline where the treacherous Ashur (Nick E. Tarabay) survives his death on Mount Vesuvius",
+    cast: ["Nick E. Tarabay", "Claudia Black", "India Shaw"],
+    episodes: [
+   { title: "Episode 1", video: "https://drive.google.com/file/d/1h3ZX-xolsi2sbS3GZc4UqNOhZLBeMMK2/preview" },
+  { title: "Episode 2", video: "https://drive.google.com/file/d/1eNw09UOxdxksgrv70rH_0L3JrGx4x34R/preview" },
+  { title: "Episode 3", video: "https://drive.google.com/file/d/1trUrYKA1d_42Yz4LpWXSbuTuwcVxT6Z1/preview" },
+  { title: "Episode 4", video: "https://drive.google.com/file/d/1ny14W-WqThR0Gkm97V1X7ZCvdhJQ7dbs/preview" },
+  { title: "Episode 5", video: "https://drive.google.com/file/d/1eVDBuaemkfDIe34V0vjIPvetrw9AUmgg/preview" },
+  { title: "Episode 6", video: "https://drive.google.com/file/d/1eQo-qvWuD6OLeqdaVR7FdTBW9u_QMg6C/preview" },
+  { title: "Episode 7", video: "https://drive.google.com/file/d/1MVDAjXAQ1IeG-cj--rXQE9ISDUQc-jxz/preview" }
 
     ]
   }
