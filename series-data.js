74f0c9a90a40ef45da5622e2d01217a07cb7f45b
busansonation <libraryyafilm@gmail.com>
2026-01-25 23:21:27 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -1,32 +1,31 @@
-
 const SERIES = [
   {
     id: "tianlan",
     title: "LEGEND OF TIANLAN",
     poster: "https://horrornews.net/wp-content/uploads/2018/03/Legend-of-the-Naga-Pearls-2017-movie-Yang-Lei-6.jpg",
-     genre: "Fantasy",
+    genre: "Fantasy",
     added: 1,
     type: "free",
     country: "China",
     year: "2026",
     synopsis: "A legendary journey of power and destiny between exorcists.",
     cast: ["Zifeng Li", "Yang Rong", "Yang Xuwen"],
-const episodes = [
-  { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
-  { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" },
-  { title: "Episode 3", video: "https://www.youtube.com/embed/UPrxaom2THM" },
-  { title: "Episode 4", video: "https://www.youtube.com/embed/squJI3jzkV4" },
-  { title: "Episode 5", video: "https://www.youtube.com/embed/LBrZEOj0w5o" },
-  { title: "Episode 6", video: "https://www.youtube.com/embed/EjXi1_0BnzE" },
-  { title: "Episode 7", video: "https://www.youtube.com/embed/mQAUHdtDbms" },
-  { title: "Episode 8", video: "https://www.youtube.com/embed/KdKOBolUC_U" },
-  { title: "Episode 9", video: "https://www.youtube.com/embed/fWngGIZ8ZLk" },
-  { title: "Episode 10", video: "https://www.youtube.com/embed/D0Op9g37vJs" },
-  { title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" }
-]
-},
+    episodes: [
+      { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
+      { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" },
+      { title: "Episode 3", video: "https://www.youtube.com/embed/UPrxaom2THM" },
+      { title: "Episode 4", video: "https://www.youtube.com/embed/squJI3jzkV4" },
+      { title: "Episode 5", video: "https://www.youtube.com/embed/LBrZEOj0w5o" },
+      { title: "Episode 6", video: "https://www.youtube.com/embed/EjXi1_0BnzE" },
+      { title: "Episode 7", video: "https://www.youtube.com/embed/mQAUHdtDbms" },
+      { title: "Episode 8", video: "https://www.youtube.com/embed/KdKOBolUC_U" },
+      { title: "Episode 9", video: "https://www.youtube.com/embed/fWngGIZ8ZLk" },
+      { title: "Episode 10", video: "https://www.youtube.com/embed/D0Op9g37vJs" },
+      { title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" }
+    ]
+  },
 
-   {
+  {
     id: "danzu",
     title: "DANZU HERO",
     poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtRri6yH0P-l6UdDL3iKisvp1FqZpEADHTaw&s",
@@ -37,9 +36,9 @@ const episodes = [
     year: "2024",
     synopsis: "A legendary journey of power and destiny between exorsits.",
     cast: ["Actor One", "Actor Two", "Actor Three"],
-episodes: [
-  { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
-  { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" }
+    episodes: [
+      { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
+      { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" }
     ]
   }
 ];
