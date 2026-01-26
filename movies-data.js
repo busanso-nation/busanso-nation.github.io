38347665e043198e5432e7b3e849569da39dc4ab
busansonation <libraryyafilm@gmail.com>
2026-01-26 01:39:45 -0800

    Update movies-data.js

--- movies-data.js
+++ movies-data.js
@@ -10,8 +10,18 @@ const MOVIES = [
     duration: "1h 45m",
     synopsis: "Bangkok-based Johnny engages in a treachery that his employer, Ramona, witnesses. She blackmails him into killing a girl, Tanya, but suddenly something unexpected happens."
   },
+    {
+    id: "underdtree",
+    title: "UNDER THE TREE",
+    poster: "https://i.ibb.co/DPQGhfgn/Untitled-1.jpg",
+    video: "https://www.youtube.com/embed/dazxm7NXbqQ",
+    type: "free",
+    year: "2026",
+    country: "Japan",
+    duration: "1h 15m",
+    synopsis: "A mysterious drama unfolding beneath an ancient tree."
+  },
 
- 
   {
     id: "kanor",
     title: "KANOR",
