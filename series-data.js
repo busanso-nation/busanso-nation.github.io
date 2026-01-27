02b592c53eb63dd896e2672147ef53ece54c8807
busansonation <libraryyafilm@gmail.com>
2026-01-27 03:31:09 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -13,16 +13,16 @@ const SERIES = [
     cast: ["Zifeng Li", "Yang Rong", "Yang Xuwen"],
     episodes: [
       { title: "Episode 1", video: "https://www.youtube.com/embed/RlNqNyrRm_k" },
-      { title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" },
-      { title: "Episode 3", video: "https://www.youtube.com/embed/UPrxaom2THM" },
-      { title: "Episode 4", video: "https://www.youtube.com/embed/squJI3jzkV4" },
-      { title: "Episode 5", video: "https://www.youtube.com/embed/LBrZEOj0w5o" },
-      { title: "Episode 6", video: "https://www.youtube.com/embed/EjXi1_0BnzE" },
-      { title: "Episode 7", video: "https://www.youtube.com/embed/mQAUHdtDbms" },
-      { title: "Episode 8", video: "https://www.youtube.com/embed/KdKOBolUC_U" },
-      { title: "Episode 9", video: "https://www.youtube.com/embed/fWngGIZ8ZLk" },
-      { title: "Episode 10", video: "https://www.youtube.com/embed/D0Op9g37vJs" },
-      { title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" }
+{ title: "Episode 2", video: "https://www.youtube.com/embed/egF1sy2FPYs" },
+{ title: "Episode 3", video: "https://www.youtube.com/embed/UPrxaom2THM" },
+{ title: "Episode 4", video: "https://www.youtube.com/embed/squJI3jzkV4" },
+{ title: "Episode 5", video: "https://www.youtube.com/embed/LBrZEOj0w5o" },
+{ title: "Episode 6", video: "https://www.youtube.com/embed/EjXi1_0BnzE" },
+{ title: "Episode 7", video: "https://www.youtube.com/embed/mQAUHdtDbms" },
+{ title: "Episode 8", video: "https://www.youtube.com/embed/KdKOBolUC_U" },
+{ title: "Episode 9", video: "https://www.youtube.com/embed/fWngGIZ8ZLk" },
+{ title: "Episode 10", video: "https://www.youtube.com/embed/D0Op9g37vJs" },
+{ title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" }
 ]
   },
 {
