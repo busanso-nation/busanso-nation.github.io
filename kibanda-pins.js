9c1b7f04ad62e78955daecbc653cc934cbc59c96
busansonation <libraryyafilm@gmail.com>
2026-01-28 15:14:22 -0800

    Update kibanda-pins.js

--- kibanda-pins.js
+++ kibanda-pins.js
@@ -1,7 +1,14 @@
-// KIBANDA USERS
-// username: { pin: "PINCODE" }
-const KIBANDA_USERS = {
-  "jojo": { pin: "ASTRA123" },
-  "desiree": { pin: "LUNA509" },
-  "elias": { pin: "VEGA920" }
+// kibanda-pins.js
+// Each username maps to a PIN
+// All usernames must be lowercase for consistency
+
+const KIBANDA_PINS = {
+  "jojo": "ASTRA123",
+  "desiree": "LUNA509",
+  "elias": "VEGA920",
+  "nick": "NICKMOV123",
+  "nasaji": "NASAJILIB456"
 };
+
+// You can add as many users as you like, e.g.:
+// "username": "PINCODE"
