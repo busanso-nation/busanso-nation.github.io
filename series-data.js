8e5cb269d530afc020e865572db168c0d7e8064b
busansonation <libraryyafilm@gmail.com>
2026-02-02 03:38:04 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -25,7 +25,8 @@ const SERIES = [
 { title: "Episode 11", video: "https://www.youtube.com/embed/ZKyROMmbIkI" },
 { title: "Episode 12", video: "https://www.youtube.com/embed/eeX5xIfwS18" },
 { title: "Episode 13", video: "https://www.youtube.com/embed/zBJN4F6D924" },
-{ title: "Episode 14", video: "https://www.youtube.com/embed/FwNLl8A8jI4" }
+{ title: "Episode 14", video: "https://www.youtube.com/embed/FwNLl8A8jI4" },
+{ title: "Episode 15", video: "https://www.youtube.com/embed/pg48xut-gkU" }
 ]
   },
 {
