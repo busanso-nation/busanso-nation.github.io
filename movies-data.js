89eaeeba96dede224199ba707fe319445b6694b1
busansonation <libraryyafilm@gmail.com>
2026-02-10 07:59:04 -0800

    Update movies-data.js

--- movies-data.js
+++ movies-data.js
@@ -1,5 +1,6 @@
+// movies-data.js
 const MOVIES = [
-    {
+  {
     id: "raksahshak",
     title: "RAKSHA KARANA",
     poster: "https://m.media-amazon.com/images/M/MV5BODEzZWNlODMtZDc0MC00N2M1LThkYWYtNGRkZDk2NWM5MjhkXkEyXkFqcGc@._V1_.jpg",
@@ -10,7 +11,7 @@ const MOVIES = [
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
-   {
+  {
     id: "raks2ahshak",
     title: "RAKSHA KARANA 2",
     poster: "https://stat4.bollywoodhungama.in/wp-content/uploads/2019/03/Kick-2.jpg",
@@ -32,7 +33,6 @@ const MOVIES = [
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
-
   {
     id: "poly2",
     title: "POLY 2",
@@ -44,15 +44,15 @@ const MOVIES = [
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
-   {
-  id: "koyla1",
+  {
+    id: "koyla1",
     title: "FIRE",
     poster: "https://cdn.cinematerial.com/p/136x/ttnsrduu/koyla-key-art-sm.jpg?v=1572983976",
     video: "https://drive.google.com/file/d/11kx2CItNZIkmSGSRVQEQg8_zXJA_hIDL/view",
     type: "premium",
     year: "2026",
     country: "India",
-    duration: "1hr 15m ",
-    synopsis: "Silenced by a piece of smouldering coal in childhood, Shanker endures a life of slavery until love compels him to seek his voice, freedom, and long-overdue revenge "
-  }, 
-    ];
+    duration: "1hr 15m",
+    synopsis: "Silenced by a piece of smouldering coal in childhood, Shanker endures a life of slavery until love compels him to seek his voice, freedom, and long-overdue revenge."
+  }
+];
