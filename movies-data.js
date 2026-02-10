8f83abe9263b2d8bac6c9e020189496416a2aff3
busansonation <libraryyafilm@gmail.com>
2026-02-10 08:37:24 -0800

    Update movies-data.js

--- movies-data.js
+++ movies-data.js
@@ -1,17 +1,17 @@
 // movies-data.js
 const MOVIES = [
-  {
+   {
     id: "raksahshak",
     title: "RAKSHA KARANA",
     poster: "https://m.media-amazon.com/images/M/MV5BODEzZWNlODMtZDc0MC00N2M1LThkYWYtNGRkZDk2NWM5MjhkXkEyXkFqcGc@._V1_.jpg",
     video: "https://www.youtube.com/embed/7XernuMVSIg",
-    type: "premium",
+    type: "free",
     year: "2026",
     country: "India",
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
-  {
+   {
     id: "raks2ahshak",
     title: "RAKSHA KARANA 2",
     poster: "https://stat4.bollywoodhungama.in/wp-content/uploads/2019/03/Kick-2.jpg",
@@ -22,17 +22,18 @@ const MOVIES = [
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
-  {
+   {
     id: "poly",
     title: "POLY",
     poster: "https://m.media-amazon.com/images/M/MV5BODEzZWNlODMtZDc0MC00N2M1LThkYWYtNGRkZDk2NWM5MjhkXkEyXkFqcGc@._V1_.jpg",
     video: "https://www.youtube.com/watch?v=Nvt3o4863fQ",
-    type: "premium",
+    type: "free",
     year: "2026",
     country: "India",
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
   },
+
   {
     id: "poly2",
     title: "POLY 2",
@@ -43,6 +44,107 @@ const MOVIES = [
     country: "India",
     duration: "1hr 15m",
     synopsis: "He is simple and polite, but he is the kind of person who can't tolerate nonsense."
+  },
+   {
+  id: "koyla1",
+    title: "FIRE",
+    poster: "https://cdn.cinematerial.com/p/136x/ttnsrduu/koyla-key-art-sm.jpg?v=1572983976",
+    video: "https://drive.google.com/file/d/11kx2CItNZIkmSGSRVQEQg8_zXJA_hIDL/view",
+    type: "premium",
+    year: "2026",
+    country: "India",
+    duration: "1hr 15m ",
+    synopsis: "Silenced by a piece of smouldering coal in childhood, Shanker endures a life of slavery until love compels him to seek his voice, freedom, and long-overdue revenge "
+  }, 
+   {
+  id: "koy2la1",
+    title: "FIRE 2",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTkyODI2NzQzMl5BMl5BanBnXkFtZTcwNzYyNDE0MQ@@._V1_.jpg",
+    video: "https://drive.google.com/file/d/1KPoozFkOrEthYrflfqT6cZFBCYocEIBS/view",
+    type: "premium",
+    year: "2026",
+    country: "India",
+    duration: "1hr 15m ",
+    synopsis: "Silenced by a piece of smouldering coal in childhood, Shanker endures a life of slavery until love compels him to seek his voice, freedom, and long-overdue revenge "
+  }, 
+    {
+  id: "kyandake",
+    title: "CHANDAKE",
+    poster: "https://bollyspice.com/wp-content/uploads/2013/10/R-1...Rajkumar-Still.jpg",
+    video: "https://www.youtube.com/embed/eqOgeaf8SFE",
+    type: "premium",
+    year: "2026",
+    country: "India",
+    duration: "1hr 10m ",
+    synopsis: "eno mbi hoooo tojiraba ngolina byokutte kuba bija kuyiika"
+  },
+     {
+  id: "cyandake",
+    title: "CHANDAKE 2",
+    poster: "https://bollyspice.com/wp-content/uploads/2013/10/R-1...Rajkumar-Still.jpg",
+    video: "https://www.youtube.com/embed/IG3jJsmz-Lc",
+    type: "premium",
+    year: "2026",
+    country: "India",
+    duration: "1hr 10m ",
+    synopsis: "eno mbi hoooo tojiraba ngolina byokutte kuba bija kuyiika"
+  },
+   {
+  
+      id: "gussa",
+    title: "GUSSA",
+    poster: "https://e1.pxfuel.com/desktop-wallpaper/300/509/desktop-wallpaper-%D0%9F%D1%80%D0%B8%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D1%91%D0%BD%D0%BD%D0%BE%D0%B5-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-south-movie-poster-thumbnail.jpg",
+    video: "https://www.youtube.com/embed/hnvEKNEiaFI",
+    type: "premium",
+    year: "2025",
+    country: "India",
+    duration: "1hr 05m ",
+    synopsis: "Based on true events, this Bollywood film depicts the Indian Army's retaliatory strikes against militant launch pads in Pakistan-administered Kashmir following the 2016 Uri attack "
+  },
+  {
+   
+      id: "gussa2",
+    title: "GUSSA 2",
+    poster: "https://e1.pxfuel.com/desktop-wallpaper/300/509/desktop-wallpaper-%D0%9F%D1%80%D0%B8%D0%BA%D1%80%D0%B5%D0%BF%D0%BB%D1%91%D0%BD%D0%BD%D0%BE%D0%B5-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-south-movie-poster-thumbnail.jpg",
+    video: "https://www.youtube.com/embed/QgkEJ77X7K0",
+    type: "premium",
+    year: "2025",
+    country: "India",
+    duration: "1hr 05m ",
+    synopsis: "Based on true events, this Bollywood film depicts the Indian Army's retaliatory strikes against militant launch pads in Pakistan-administered Kashmir following the 2016 Uri attack "
+  },
+  {
+      id: "kaampa",
+    title: "KAAMPANA",
+    poster: "https://static.toiimg.com/photo/112748464/112748464.jpg",
+    video: "https://www.youtube.com/embed/JATaCk17Bjk",
+    type: "premium",
+    year: "2025",
+    country: "India",
+    duration: "1hr 05m ",
+    synopsis: "These films feature larger-than-life protagonists, high-stakes fight choreography, and frequently, a mix of romance and emotional storylines.  "
+  },
+  {
+       id: "kaampana2",
+    title: "KAAMPANA 2",
+    poster: "https://static.toiimg.com/photo/112748464/112748464.jpg",
+    video: "https://drive.google.com/file/d/10MzGiTgY-KnE07iGtXN6HSY1k1YJmk0q/preview",
+    type: "premium",
+    year: "2025",
+    country: "India",
+    duration: "1hr 05m ",
+    synopsis: "These films feature larger-than-life protagonists, high-stakes fight choreography, and,,, frequently, a mix of romance and emotional storylines. "
+  },
+  {
+    id: "radhee",
+    title: "RADHE",
+    poster: "https://i.ibb.co/SDjNgMyH/Chat-GPT-Image-Feb-6-2026-01-26-06-PM.png",
+    video: "https://www.youtube.com/embed/7riCt-H911w",
+    type: "premium",
+    year: "2026",
+    country: "India",
+    duration: "1hr 05m ",
+    synopsis: "Radhe is a poor Hindu with a bad temper, who often gets into fights. One of these fights puts him in the hospital with head injuries causing amnesia. At the same time a woman he was in love with re-enters his life. "
   },
   {
     id: "koyla1",
