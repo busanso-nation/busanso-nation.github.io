6454eb9416194b49fbfcc194702dbb5622d3cbd6
busansonation <libraryyafilm@gmail.com>
2026-02-13 01:48:39 -0800

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -77,7 +77,8 @@ const SERIES = [
 { title: "Episode 22", video: "https://drive.google.com/file/d/14bVKJ7XQz8QsQrwcBoM4RjPB2DfxMiGD/preview" },
  { title: "Episode 23", video: "https://drive.google.com/file/d/1dB9VYbPbWxstjqdra5BG4Iqvu3bamszC/preview" },
 { title: "Episode 24", video: "https://drive.google.com/file/d/1LDu_TFyIBlFoCaweBLyYGRswXsmxlNaG/preview" },
-{ title: "Episode 25", video: "https://drive.google.com/file/d/1PRyQgZE9Ko3tfJmdS3ZUr9UreSsfDP-w/preview" }
+{ title: "Episode 25", video: "https://drive.google.com/file/d/1PRyQgZE9Ko3tfJmdS3ZUr9UreSsfDP-w/preview" },
+{ title: "Episode 26", video: "https://drive.google.com/file/d/1XVORpx0zFi95WZu6-1AM8mPHWDgWt90m/preview" }
       ]
   },
 {
